0274b3b3899f8f8b8f927795a13ad9fdaa215ee4
Влад <rtlistate@gmail.com>
2019-12-15 23:44:05 +0300

    Добавляет стили для футера проекта

--- build/js/popup-menu.js
+++ build/js/popup-menu.js
@@ -1,11 +1,18 @@
 'use strict';
 
 (function () {
+  var mainPage = document.querySelector('.main-page');
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
+  var main = document.querySelector('.main');
+  var pageFooter = document.querySelector('.page-footer');
 
   function onClickHeaderToggle() {
+    mainPage.classList.toggle('main-page--menu-opened');
     pageHeader.classList.toggle('page-header--opened');
+    main.classList.toggle('main--hidden');
+    pageFooter.classList.toggle('page-footer--hidden');
+    pageFooter.classList.toggle('page-footer--menu-opened');
   }
 
   headerToggle.addEventListener('click', onClickHeaderToggle);
